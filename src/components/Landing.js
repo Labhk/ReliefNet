fbcf7310e7dea85bd6c7a94ae20a78b0cf999f7a
earny4302 <73746161+earny4302@users.noreply.github.com>
2023-02-17 19:36:15 +0530

    new

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,15 +6,15 @@ import { Link } from 'react-router-dom'
 const Landing = () => {
   return (
     <div class="LandingPage">
-        <div class="content">
-            <h1 class="content_Landing_h">Connecting <br/>Relief Camps <br/>in Crisis</h1>
-            <p>ReliefNet connects relief camps during times of<br/> crisis, making sure the most afftected people receive<br/>help when they need it the most.</p>
-        </div>
-        <div>
+        
+            <h class="content_Landing_h">Connecting Relief Camps <br/>in Crisis</h>
+            <p class ="content">ReliefNet connects relief camps during times of crisis, making sure the most afftected people receive help when they need it the most.</p>
+        
+        
 
         <img class="Landing_img" src="https://i.ibb.co/zngdCTK/Landingpage-donation.png" alt="My-project" border="0"/>
        
-        </div>
+        
        
     </div>
   )
